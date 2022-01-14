1fda64489416d8feac3964dd4e3bfd158f762fc3
Evan Verney-Fink <evan@triskel-inc.com>
2022-01-14 15:11:19 -0500

    Fix crash * Remove unused Id, DateCreated, and DateModified properties in ResourceEndpoint * Make AuthType and AuthContext pointers to allow for null values

--- model/resourceendpoint.go
+++ model/resourceendpoint.go
@@ -1,14 +1,9 @@
 package model
 
-import "time"
-
 type ResourceEndpoint struct {
-	Type         string      `json:"@type"`
-	Id           string      `json:"id"`
-	DateCreated  time.Time   `json:"dateCreated"`
-	DateModified time.Time   `json:"dateModified"`
-	ResourceType string      `json:"resourceType"`
-	HttpEndpoint string      `json:"httpEndpoint"`
-	AuthType     string      `json:"authType"`
-	AuthContext  interface{} `json:"authContext"`
+	Type         string       `json:"@type"`
+	ResourceType string       `json:"resourceType"`
+	HttpEndpoint string       `json:"httpEndpoint"`
+	AuthType     *string      `json:"authType"`
+	AuthContext  *interface{} `json:"authContext"`
 }
